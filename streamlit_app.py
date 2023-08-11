9dfc409abc39878c0288f84935d4989c4348c1e9
Martin Stanek <info@martinstanek.cz>
2023-08-11 10:58:39 +0200

    Fix(typo): redundant generate_response method

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,34 +10,18 @@ import faiss
 import pickle
 
 
-
-def generate_response(openai_api_key, query_text):
-    # Load the LangChain.
-    index = faiss.read_index("docs.index")
-
-    with open("faiss_store.pkl", "rb") as f:
-        store = pickle.load(f)
-
-    store.index = index
-    chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0.2, openai_api_key=openai_api_key), vectorstore=store)
-    result = chain({"question": query_text})
-    # print(f"{result['answer']}")
-    # print()
-    # print(f"Zdroje: {result['sources']}")
-
-    return result['answer']
-    # # Split documents into chunks
-    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    # texts = text_splitter.create_documents(documents)
-    # # Select embeddings
-    # embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # # Create a vectorstore from documents
-    # db = Chroma.from_documents(texts, embeddings)
-    # # Create retriever interface
-    # retriever = db.as_retriever()
-    # # Create QA chain
-    # qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    # return qa.run(query_text)
+# # Split documents into chunks
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.create_documents(documents)
+# # Select embeddings
+# embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+# # Create a vectorstore from documents
+# db = Chroma.from_documents(texts, embeddings)
+# # Create retriever interface
+# retriever = db.as_retriever()
+# # Create QA chain
+# qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+# return qa.run(query_text)
 
 
 # App title
