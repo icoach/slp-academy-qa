99230e79bdcad15479d74021bf0012e0eb8fd0ab
Martin Stanek <info@martinstanek.cz>
2023-08-11 09:59:59 +0200

    Fix(db): replace faiss with faiss-cpu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.chains import VectorDBQAWithSourcesChain
 
-import faiss
+import faiss-cpu
 import pickle
 
 
